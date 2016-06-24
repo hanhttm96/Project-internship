e0a1a24c0d41ca51473399fa270988b8035e819c
Tran Thi My Hanh <myhanhqt96@gmail.com>
2016-06-24 17:09:37 +0700

    24/6

--- Internship/bootstrap/js/color.js
+++ Internship/bootstrap/js/color.js
@@ -20,25 +20,27 @@ $(function(){
               event.preventDefault();
               $('#CartCount').attr('class', 'badge-green');
               $('.header-main').attr('class', 'header-main fixed border-green');
+              $("a").attr('class', 'a-color-green');
             }
 
             function switchPink(event) {
               event.preventDefault();
               $('#CartCount').attr('class', 'badge-pink');
               $('.header-main').attr('class', 'header-main fixed border-pink');
-           
+              $("a").attr('class', 'a-color-pink');
             }
 
             function switchBlue(event) {
               event.preventDefault();
               $('#CartCount').attr('class', 'badge-blue');
               $('.header-main').attr('class', 'header-main fixed border-blue');
+              $("a").attr('class', 'a-color-blue');
             }
             function switchBlack(event) {
               event.preventDefault();
               $('#CartCount').attr('class', 'badge-black');
               $('.header-main').attr('class', 'header-main fixed border-black');
-           
+              $("a").attr('class', 'a-color-black');
          
             }
 
