34b5eda3ef65fdca681a91a2e2db8a561359b24d
Tran Thi My Hanh <myhanhqt96@gmail.com>
2016-06-30 16:30:46 +0700

    30/6

--- Internship/bootstrap/js/color.js
+++ Internship/bootstrap/js/color.js
@@ -21,6 +21,7 @@ $(function(){
               $('#CartCount').attr('class', 'badge-green');
               $('.header-main').attr('class', 'header-main fixed border-green');
               $("a").attr('class', 'a-color-green');
+              $("h3").attr('class', 'h-green');
             }
 
             function switchPink(event) {
@@ -28,6 +29,7 @@ $(function(){
               $('#CartCount').attr('class', 'badge-pink');
               $('.header-main').attr('class', 'header-main fixed border-pink');
               $("a").attr('class', 'a-color-pink');
+              $("h3").attr('class', 'h-pink');
             }
 
             function switchBlue(event) {
@@ -35,12 +37,14 @@ $(function(){
               $('#CartCount').attr('class', 'badge-blue');
               $('.header-main').attr('class', 'header-main fixed border-blue');
               $("a").attr('class', 'a-color-blue');
+              $("h3").attr('class', 'h-blue');
             }
             function switchBlack(event) {
               event.preventDefault();
               $('#CartCount').attr('class', 'badge-black');
               $('.header-main').attr('class', 'header-main fixed border-black');
               $("a").attr('class', 'a-color-black');
+              $("h3").attr('class', 'h-black');
          
             }
 
