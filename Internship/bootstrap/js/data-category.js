f1b74584fa45a6bb35adae4c0f7863ead4746a89
Tran Thi My Hanh <myhanhqt96@gmail.com>
2016-08-11 15:10:50 +0700

    11/8

--- Internship/bootstrap/js/data-category.js
+++ Internship/bootstrap/js/data-category.js
@@ -1,7 +1,7 @@
 var products = [
     {prod: 'Áo'},
     {prod: 'Phụ kiện'},
-    {prod: 'Váy, đầm'},
+    {prod: 'Váy, đầm'}
 ];
 
 var trademarks = [
@@ -9,54 +9,54 @@ var trademarks = [
     {name: 'Calvin Klein'},
     {name: 'Charter Club'},
     {name: 'Cosabella'},
-    {name: 'DKNY'},
+    {name: 'DKNY'}
 ];
 var prices = [
     {level: 'Dưới 1 triệu'},
     {level: '1 triệu - 2 triệu'},
     {level: '2 triệu - 5 triệu'},
     {level: '5 triệu - 10 triệu'},
-    {level: 'Trên 10 triệu'},
+    {level: 'Trên 10 triệu'}
 ];
 var prod_new_left = [
     {title_prod: 'Áo Len Ren Skinny...', price: '740.000₫', imageUrl: "bootstrap/img/sp1.jpeg"},
     {title_prod: 'Váy Hoa Nữ A32-W122', price: '5.200.000₫', imageUrl: "bootstrap/img/sp5.jpeg"},
-    {title_prod: 'Váy Skinny Túi Viền...', price: '5.200.000₫', imageUrl: "bootstrap/img/sp3.jpeg"},
+    {title_prod: 'Váy Skinny Túi Viền...', price: '5.200.000₫', imageUrl: "bootstrap/img/sp3.jpeg"}
 ];
 var prod_new_right = [
     {title_prod: 'Chân Váy Chữ A...', price: '9.800.000₫', imageUrl: "bootstrap/img/sp8.jpeg"},
     {title_prod: 'Đầm Xòe Michael Kors', price: '8.600.000₫', imageUrl: "bootstrap/img/sp4.jpeg"},
-    {title_prod: 'Váy Ren Tommy Hilfiger', price: '11.500.000₫', imageUrl: "bootstrap/img/sp2.jpeg"},
+    {title_prod: 'Váy Ren Tommy Hilfiger', price: '11.500.000₫', imageUrl: "bootstrap/img/sp2.jpeg"}
 ];
 var regular_slider = [
     {imageUrl: "bootstrap/img/vendor_logo_1.png"},
     {imageUrl: "bootstrap/img/vendor_logo_2.png"},
     {imageUrl: "bootstrap/img/vendor_logo_3.png"},
     {imageUrl: "bootstrap/img/vendor_logo_4.png"},
-    {imageUrl: "bootstrap/img/vendor_logo_5.png"},
+    {imageUrl: "bootstrap/img/vendor_logo_5.png"}
 ];
 var vay_mi_sliders = [
     {vay: 'Boots', price: '5.500.000đ', imageUrl: "bootstrap/images/1.jpg"},
     {vay: 'Oxfords', price: '8.600.000₫', imageUrl: "bootstrap/images/2.jpg"},
     {vay: 'Loafers', price: '11.500.000₫', imageUrl: "bootstrap/images/3.jpg"},
-    {vay: 'Sneakers', price: '9.800.000₫', imageUrl: "bootstrap/images/4.jpg"},
+    {vay: 'Sneakers', price: '9.800.000₫', imageUrl: "bootstrap/images/4.jpg"}
 ];
 var quan_mi_sliders = [
     {quan: 'Belts', price: '700.000₫', imageUrl: "bootstrap/images/5.jpg"},
     {quan: 'Hats & Caps', price: '800.000₫', imageUrl: "bootstrap/images/6.jpg"},
     {quan: 'Sunglasses', price: '400.000đ', imageUrl: "bootstrap/images/7.jpg"},
-    {quan: 'Scarves', price: '500.000₫', imageUrl: "bootstrap/images/8.jpg"},
+    {quan: 'Scarves', price: '500.000₫', imageUrl: "bootstrap/images/8.jpg"}
 ];
 var pk_mi_sliders = [
     {pk: 'Belts', price: '700.000₫', imageUrl: "bootstrap/images/9.jpg"},
     {pk: 'Hats & Caps', price: '800.000₫', imageUrl: "bootstrap/images/10.jpg"},
-    {pk: 'Sunglasses', price: '400.000đ', imageUrl: "bootstrap/images/11.jpg"},
+    {pk: 'Sunglasses', price: '400.000đ', imageUrl: "bootstrap/images/11.jpg"}
 ];
 var giay_mi_sliders = [
     {giay: 'Carry-Ons', price: '600.000₫', imageUrl: "bootstrap/images/12.jpg"},
     {giay: 'Duffel Bags', price: '500.000₫', imageUrl: "bootstrap/images/13.jpg"},
     {giay: 'Laptop Bags', price: '400.000đ', imageUrl: "bootstrap/images/14.jpg"},
-    {giay: 'Briefcases', price: '550.000đ', imageUrl: "bootstrap/images/15.jpg"},
+    {giay: 'Briefcases', price: '550.000đ', imageUrl: "bootstrap/images/15.jpg"}
 ];
 var footers = {
     slogan: 'Fashion chuyên cung cấp các mẫu thời tràn phù hợp cho thời trang công sở. Cam kết mang lại chất lượng sản phẩm cao, mẫu mã đẹp hợp thời trang. Chúng tôi luôn tiếp nhận những thông tin của khách hàng để cải thiên khả năng phục vụ và chất lượng sản phẩm',
@@ -72,7 +72,7 @@ var infomation_prods = [
     {gioi_thieu: '- Đường may chắc chắn, khéo léo.'},
     {gioi_thieu: '- Dễ dàng mix cùng các kiểu áo form dài như sơ mi, áo thun ... '},
     {gioi_thieu: '- Chất liệu: Jeans co giãn tốt, tạo cảm giác dễ chịu khi di chuyển.'},
-    {gioi_thieu: '- Size: XL - XXL - XXXL - XXXXL'},
+    {gioi_thieu: '- Size: XL - XXL - XXXL - XXXXL'}
 ];
 var policys = [
     {dieu: 'Với mong muốn mang lại sự hài lòng cho quý khách khi mua hàng, chúng tôi có những quy định trong vận chuyển, nhằm đảm bảo rằng những sản phẩm quý khách mua là sản phẩm mà vừa ý nhất.'},
@@ -87,27 +87,27 @@ var policys = [
     {dieu: '6. Trường hợp không đúng địa chỉ, thay đổi địa chỉ:'},
     {dieu: '- Không đúng địa chỉ: trường hợp sai địa chỉ chúng tôi sẽ lưu lại 6 tiếng và liên lạc với người gửi và người nhận để thỏa thuận về địa điểm, thời gian, nếu địa chỉ mới không quá 3km sẽ phát miễn phí. Trường hợp địa chỉ mới xa hơn 3km sẽ tính thêm phí theo quy định chuyển phát.'},
     {dieu: '7. Trường hợp không tồn tại người nhận tại địa chỉ yêu cầu: đơn hàng sẽ được hủy và không được hoàn lại thanh toán.'},
-    {dieu: '8. Chúng tôi không vận chuyển sản phẩm đến các địa chỉ trên tàu hỏa, máy bay, tàu thủy, khu vực nguy hiểm, các khu vực cấm…'},
+    {dieu: '8. Chúng tôi không vận chuyển sản phẩm đến các địa chỉ trên tàu hỏa, máy bay, tàu thủy, khu vực nguy hiểm, các khu vực cấm…'}
 ];
 var top1 = [
-    {id: 1, title_product: 'Áo Len Ren Skinny...', imageUrl: "bootstrap/img/sp3.jpeg", },
+    {id: 1, title_product: 'Áo Len Ren Skinny...', imageUrl: "bootstrap/img/sp3.jpeg" },
     {id: 2, title_product: 'Váy Hoa Nữ A32-W122', imageUrl: "bootstrap/img/sp5.jpeg"},
-    {id: 3, title_product: 'Váy Ren Tommy Hilfiger', imageUrl: "bootstrap/img/sp2.jpeg"},
+    {id: 3, title_product: 'Váy Ren Tommy Hilfiger', imageUrl: "bootstrap/img/sp2.jpeg"}
 ];
 var top2 = [
     {id: 4, title_product: 'Chân Váy Chữ A...', imageUrl: "bootstrap/img/sp8.jpeg"},
     {id: 5, title_product: 'Váy 1 Vai Skinny...', imageUrl: "bootstrap/img/sp9.jpeg"},
-    {id: 6, title_product: 'Váy Hoa Nữ A32-W122', imageUrl: "bootstrap/img/sp1.jpeg"},
+    {id: 6, title_product: 'Váy Hoa Nữ A32-W122', imageUrl: "bootstrap/img/sp1.jpeg"}
 ];
 var top3 = [
     {id: 7, title_product: 'Đầm Xòe Michael Kors', imageUrl: "bootstrap/img/sp4.jpeg"},
     {id: 8, title_product: 'Váy Ren Tommy Hilfiger', imageUrl: "bootstrap/img/sp5.jpeg"},
-    {id: 9, title_product: 'Váy Skinny Túi Viền...', imageUrl: "bootstrap/img/sp3.jpeg"},
+    {id: 9, title_product: 'Váy Skinny Túi Viền...', imageUrl: "bootstrap/img/sp3.jpeg"}
 ];
 var top4 = [
     {id: 10, title_product: 'Váy Ren Tommy Hilfiger', imageUrl: "bootstrap/img/sp2.jpeg"},
     {id: 11, title_product: 'Áo Len Ren Skinny...', imageUrl: "bootstrap/img/sp9.jpeg"},
-    {id: 12, title_product: 'Váy Hoa Nữ A32-W122', imageUrl: "bootstrap/img/sp1.jpeg"},
+    {id: 12, title_product: 'Váy Hoa Nữ A32-W122', imageUrl: "bootstrap/img/sp1.jpeg"}
 ];
 
 var titles = [
@@ -122,7 +122,7 @@ var titles = [
     {id: 9, title_product: 'Váy Skinny Túi Viền...', price: '5.200.000₫', imageUrl: "bootstrap/img/sp3.jpeg"},
     {id: 10, title_product: 'Váy Ren Tommy Hilfiger', price: '11.500.000₫', imageUrl: "bootstrap/img/sp2.jpeg"},
     {id: 11, title_product: 'Áo Len Ren Skinny...', price: '740.000₫', imageUrl: "bootstrap/img/sp9.jpeg"},
-    {id: 12, title_product: 'Váy Hoa Nữ A32-W122', price: '5.200.000₫', imageUrl: "bootstrap/img/sp1.jpeg"},
+    {id: 12, title_product: 'Váy Hoa Nữ A32-W122', price: '5.200.000₫', imageUrl: "bootstrap/img/sp1.jpeg"}
 ];
 var citys = [
     {value: 2, name: "TP Hồ Chí Minh"},
@@ -147,8 +147,7 @@ var citys = [
     {value: 21, name: "Đồng Nai"},
     {value: 22, name: "Đồng Tháp"},
     {value: 23, name: "Gia Lai"},
-    {value: 24, name: "Hà Giang"},
-    
+    {value: 24, name: "Hà Giang"}
 ];
 var countys = [
     {value: 1, name: "Quận Hoàn Kiếm"},
@@ -174,8 +173,7 @@ var countys = [
     {value: 21, name: "Huyện Quốc Oai"},
     {value: 22, name: "Huyện Sóc Sơn"},
     {value: 23, name: "Huyện Thạch Thất"},
-    {value: 24, name: "Huyện Thanh Oai"},
-    
+    {value: 24, name: "Huyện Thanh Oai"}
 ];
 //ui-router
 var fashionApp = angular.module('fashionApp', ['ui.router', 'ngDialog']);
@@ -276,7 +274,7 @@ function homeController($scope, $timeout) {
 
 }
 
-fashionApp.controller("menuCtrl", menuCtrl);
+
 fashionApp.controller("repeatCategory", repeatCategory);
 function repeatCategory($scope, $rootScope) {
 
@@ -311,15 +309,110 @@ function menuCtrl($scope, $rootScope, ngDialog, $timeout) {
         $scope.openSearch = !$scope.openSearch;
 
     }
-    //popup
+//popup cart
     $scope.open = function () {
         console.log("gsfs");
-        ngDialog.open({template: 'bootstrap/popup/popupCart.html', className: 'ngdialog-theme-default'});
+        ngDialog.open({
+            template: 'bootstrap/popup/popupCart.html',
+            className: 'ngdialog-theme-default',
+            controller: 'menuCtrl'
+        });
     };
-
 }
 
+fashionApp.controller("menuCtrl", menuCtrl);
 
+
+
+fashionApp.controller('userCtrl', ['$scope', function ($scope) {
+        var self = this;
+        self.user = {id: null, username: '', password: '', email: ''};
+        self.id = 1;
+        self.users = [];
+        self.submit = function () {
+            if (self.user.id === null) {
+                self.user.id = self.id++;
+                console.log('Saving New User', self.user);
+                self.users.push(self.user); //Or send to server, we will do it in when handling services
+
+            } else {
+                for (var i = 0; i < self.users.length; i++) {
+                    if (self.users[i].id === self.user.id) {
+                        self.users[i] = self.user;
+                        break;
+                    }
+                }
+                console.log('User updated with id ', self.user.id);
+            }
+            self.reset();
+        };
+        self.edit = function (id) {
+            console.log('id to be edited', id);
+            for (var i = 0; i < self.users.length; i++) {
+                if (self.users[i].id === id) {
+                    self.user = angular.copy(self.users[i]);
+                    break;
+                }
+            }
+        }
+
+        self.reset = function () {
+            self.user = {id: null, username: '', password: '', email: ''};
+            $scope.myForm.$setPristine(); //reset Form
+        }
+
+    }]);
+fashionApp.controller("userCtrl", userCtrl);
+//popup user
+function userCtrl($scope, $rootScope, ngDialog, $timeout){
+    $scope.currentTab = 'login';
+    $scope.id = 1;
+    $scope.changeTab = function(key){
+        $scope.currentTab = key;
+    }
+    $scope.open = function () {
+        console.log('nhan');
+        ngDialog.open({
+            template: 'bootstrap/popup/popupLogin.html',
+            className: 'ngdialog-theme-default',
+            controller: 'userCtrl'
+        });
+    };
+    $scope.user = {id: null, username: '', password: '', email: ''};
+    $scope.users = [];
+        
+        $scope.submit = function () {
+            if ($scope.user.id === null) {
+                $scope.user.id = $scope.id++;
+                console.log('Saving New User', $scope.user);
+                $scope.users.push($scope.user); //Or send to server, we will do it in when handling services
+
+            } else {
+                angular.forEach($scope.users,function(value,index){
+                    if(value.id === $scope.user.id){
+                        value = $scope.user;
+                    }
+                });
+                
+            }
+            $scope.reset();
+        };
+        $scope.edit = function (id) {
+            console.log('id to be edited', id);
+            for (var i = 0; i < $scope.users.length; i++) {
+                if ($scope.users[i].id === id) {
+                    $scope.user = angular.copy($scope.users[i]);
+                    break;
+                }
+            }
+        }
+
+        $scope.reset = function () {
+            $scope.user = {id: null, username: '', password: '', email: ''};
+            ngDialog.closeAll();
+            console.log('Saving New User', $scope.users);
+        }
+}
 fashionApp.controller('namesCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
 
         $scope.orderByMe = function (x) {
@@ -363,7 +456,6 @@ fashionApp.filter('filterFunction', function () {
     }
 });
 
-
 fashionApp.controller('AppController', ['$scope', function ($scope) {
         var self = this;
         self.user = {id: null, username: '', password: '', email: ''};
@@ -402,3 +494,4 @@ fashionApp.controller('AppController', ['$scope', function ($scope) {
         }
 
     }]);
+
